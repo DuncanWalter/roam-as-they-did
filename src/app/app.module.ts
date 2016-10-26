c129870bf9898be9ce33ee71c4ef19845e76699c
Duncan Walter <dwalter@barlingarts.com>
2016-10-26 16:16:28 -0500

    Added the profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RoamProfileComponent } from './roam-profile/roam-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoamProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/roam-profile/roam-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'roam-profile',
+  templateUrl: './roam-profile.component.html',
+  styleUrls: ['./roam-profile.component.css']
+})
+export class RoamProfileComponent {
+
+}
